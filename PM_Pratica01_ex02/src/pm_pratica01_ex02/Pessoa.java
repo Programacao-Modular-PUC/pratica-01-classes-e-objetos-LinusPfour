e8c63be26e94aa2f22f318ce7a461e9dabcf2017
LinusPfour <luizpaulo.delgado@yahoo.com.br>
2026-03-16 00:41:50 -0300

    Prática01_ClassificaçãoCorporal

---
+++ PM_Pratica01_ex02/src/pm_pratica01_ex02/Pessoa.java
@@ -0,0 +1,92 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pm_pratica01_ex02;
+
+public class Pessoa {
+
+    private String nome;
+    private String sobrenome;
+    private int idade;
+    private double altura;
+    private double peso;
+    private double imc;
+
+    public Pessoa(String nome, String sobrenome, int idade, double altura, double peso) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getImc() {
+        return imc;
+    }
+
+    public void setImc(double imc) {
+        this.imc = imc;
+    }
+
+    public double calculaIMC() {
+        imc = peso / (altura * altura);
+        return imc;
+    }
+
+    public String informaObesidade() {
+        if (imc < 18.5) {
+            return "Abaixo do peso";
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            return "Peso normal";
+        } else if (imc <= 29.9) {
+            return "Sobrepeso";
+        } else if (imc <= 34.9) {
+            return "Obesidade grau 1";
+        } else if (imc <= 39.9) {
+            return "Obesidade grau 2";
+        } else {
+            return "Obesidade grau 3";
+        }
+    }
+}
